720b4fa7ba2f44a1182b0da5d43f6fc7c0193952
lucasbaruj4 <barujalucas0@gmail.com>
2025-11-15 11:29:49 +0100

    going back to using esc for mode-changing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "R", "i")
-vim.keymap.set("i", "R", "<Esc>")
-vim.keymap.set("v", "R", "<Esc>")
